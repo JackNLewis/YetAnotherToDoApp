64ca9640dc26c7fcb81e59fb5c45110f71df61ba
jack lewis <jacklewis@jacks-Laptop.local>
2024-02-21 22:24:19 +0000

    got animations working

--- src/InputTask.tsx
+++ src/InputTask.tsx
@@ -1,13 +1,33 @@
 import * as React from 'react';
-import { HStack, Input, Button, Icon, useColorMode } from '@chakra-ui/react';
+import { HStack, Input, Button, Icon, useColorMode, Box } from '@chakra-ui/react';
 
 
 export default function InputTask(){
     const { colorMode, toggleColorMode } = useColorMode()
+    const [yOffset, setYoffset] = React.useState('0vh');
+
+    let customCSS = {
+        'caretColor': colorMode == 'light' ? 'darkgrey' : 'darkgrey',
+    }
+    
+    let transitionCss = {
+        'transition' : 'transform 2s'
+    }
+
     return (
-        <HStack>
-            <Input placeholder='enter task'  variant='unstyled' size='lg' autoFocus={true} 
-            sx={{'caret-color': colorMode == 'light' ? 'darkgrey' : 'darkgrey' }}/>
+        <HStack height={'100vh'}>
+            <Box position='relative'
+                top={yOffset}
+                transition='top 0.5s linear'>
+            <Input
+                placeholder='enter task'
+                variant='flushed'
+                size='lg'
+                onInput={() => setYoffset('40vh')}
+                autoFocus={true}
+                sx={customCSS}
+                />
+            </Box>
         </HStack>
     )
 }
